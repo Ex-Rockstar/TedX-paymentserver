94c00cc70ce8eccaa051733690c7b3b7bbf1bf22
Ex-Rockstar <ruthirockstar2218@gmail.com>
2026-02-12 15:34:48 +0530

    added kanish 4

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.post("/buy-ticket", async (req, res) => {
     })
 
     /* Generate UPI QR */
-    const upi = "9025955248@ybl" 
+    const upi = "kanishsbi@ybl" 
     const upiLink =
       `upi://pay?pa=${upi}` +
       `&pn=TEDxSairam` +
