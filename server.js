ad310ec139ab12bd9d20c23c213f4657071c806e
Ex-Rockstar <ruthirockstar2218@gmail.com>
2026-02-12 12:32:33 +0530

    sailendravel

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.post("/buy-ticket", async (req, res) => {
     })
 
     /* Generate UPI QR */
-    const upi = "kanishubi@axl"
+    const upi = "niveshkarthik125@oksbi"
     const upiLink =
       `upi://pay?pa=${upi}` +
       `&pn=TEDxSairam` +
